731b204efcce6ea8118e1d68394bd074080d1187
tamerhayek <dev@tamerhayek.com>
2024-08-19 10:12:15 +0200

    feat: rotate player

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 # Base class for game objects
 class Player(CircleShape):
@@ -20,9 +20,16 @@ class Player(CircleShape):
 		c = self.position - forward * self.radius + right
 		return [a, b, c]
 
+	def rotate(self, dt):
+		self.rotation += PLAYER_TURN_SPEED * dt
+
 	def draw(self, screen):
 		pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
 	def update(self, dt):
-		# sub-classes must override
-		pass
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_a]:
+			self.rotate(-dt)
+		if keys[pygame.K_d]:
+			self.rotate(dt)
