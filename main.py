f83f025c8b5fe989f1b908d5ff7db027c74834d1
tamerhayek <dev@tamerhayek.com>
2024-08-20 09:51:49 +0200

    feat: create asteroids

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -19,10 +21,14 @@ def main():
 
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
 
 	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
 
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
 
 	while True:
 		for event in pygame.event.get():
--- player.py
+++ player.py
@@ -7,8 +7,6 @@ from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 class Player(CircleShape):
 	def __init__(self, x, y):
 		super().__init__(x, y, PLAYER_RADIUS)
-		self.position = pygame.Vector2(x, y)
-		self.radius = PLAYER_RADIUS
 		self.rotation = 0
 
 	# in the player class
